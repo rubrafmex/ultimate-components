5cc9890ec932c96aec0795f634de208e3860a8ac
Ruben Rafael Martinez <89-rrmadmin@users.noreply.gitlab.cmpayments.local>
2022-04-25 17:54:46 +0200

    JavaScript closures and the useEffect hook - useEffect with errors: 1 because of closures (which make our code to print always 1) and with time variable, it needs to be added to the dependency array since it is declared in the scope of our component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './machine.css';
-import Counter4 from "./Counter4";
+import StopwatchWithErrors from "./StopwatchWithErrors";
 
 const App = () => {
     return (
         <main>
-            <h1>Ultimate Counter</h1>
-            <Counter4/>
+            <StopwatchWithErrors/>
         </main>
     );
 };
