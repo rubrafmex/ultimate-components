32c6d18e0374ed5200d8d1890a1fa2095ba685f7
Ruben Rafael Martinez <89-rrmadmin@users.noreply.gitlab.cmpayments.local>
2022-04-25 17:19:01 +0200

    Cleaning up after side effects - version with non empty dependency array, to run side effect and clean up with every change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './machine.css';
 import Counter4 from "./Counter4";
 
 const App = () => {
-    const [show, setShow] = useState(true)
-
-    const onClickHandler = () => {
-        setShow(s => !s)
-    }
-
     return (
         <main>
             <h1>Ultimate Counter</h1>
-            {show && <Counter4/>}
-            <button type="button" onClick={onClickHandler} aria-pressed={!show}>
-                Hide/Show
-            </button>
+            <Counter4/>
         </main>
     );
 };
--- src/Counter4.js
+++ src/Counter4.js
@@ -1,11 +1,11 @@
 import React, {Fragment, useState, useEffect} from "react";
 
-const subscribe = () => {
-    console.log('subscribed');
+const subscribe = count => {
+    console.log(`Subscribed for ${count}`);
 };
 
-const unsubscribe = () => {
-    console.log('unsubscribed');
+const unsubscribe = count => {
+    console.log(`Unsubscribed for ${count}`);
 };
 
 const Counter4 = () => {
@@ -18,24 +18,12 @@ const Counter4 = () => {
     );
 
     useEffect(() => {
-            subscribe();
-
-            // how about to execute side effect code when a component is unmounted, that is when it is removed from the React dom and from the browser dom
-            // If we return a function from our useEffect, this becomes a cleaning function
+            subscribe(counter)
             return () => {
-                unsubscribe()
+                unsubscribe(counter)
             };
-
-        }, [] // will make to run our side effect only once when the component is mounted.
-        //
-        // Why we did not have to put subscribe function inside the dependency array?
-        //
-        // Because it was declared outside the render scope of the Counter4 component, it is not part of the
-        // component function, and therefore we can treat it as a true constant and does not have to be part of this dependency array.
-        //
-        // This is also a common practice, if we have a side effects that does not use anything inside of our components, it is often a good idea
-        // to move it outside of the component, making code easier to read. Just like what we have here, using a function that is declared outside
-        // our component.
+        },
+        [counter]
     );
 
     const onCountClickHandler = () => {
